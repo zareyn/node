0fdcecc2926c32bf8c1414cd9492169ce35616b0
zareyn <137310368+zareyn@users.noreply.github.com>
2025-01-27 20:48:14 +0300

    debug test-demo.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,60 @@
 import express, { Request, Response } from "express"
+import { connect } from "http2";
+//import { Pool } from "mysql2/typings/mysql/lib/Pool";
 import path from "path";
-import mysql from "mysql";
-
-
-const connection = mysql.createConnection({
+//import mysql from "mysql";
+import pg, { Client } from 'pg';
+ 
+const connection = new pg.Client({
   port: 5444,
-  host: "localhost",
-  user: "test",
-  database: "public",
-  password: "test",
-}); 
+  user: 'test',
+  password: 'test',
+  host: 'localhost',
+  database: 'test',
+})
 
- connection.connect(err =>{
+//const connection = mysql.createConnection({
+//  port: 5444,
+//  host: "localhost",
+//  user: "test",
+//  database: "test",
+//  password: "test",
+//}); 
+
+  connection.connect(err =>{
     if (err) {
-      return console.error("Server Clfame error: " + err.message);
+     return console.error("- ☺ Server Clfame error: " + err.message);
     }
     else{
-      console.log("Подключение успешно установлено");
+      console.log("+ ☺ Server Clfame connect");
     }
- });
-//connection.query("SELECT * FROM users",
-   // function(err:Error) {
-   //   console.log(err.message); 
- // });
- // const sql = "INSERT INTO users(user, password) VALUES(demo, demo)";
+});
+  connection.query("SELECT * FROM public.users", 
+    (err:Error) => { if (err){
+     console.log("- ☺ file users Clfame error: " + err.message)}else
+    {console.log("+ ☺ file users Clfame connect")}});
+ connection.query("INSERT INTO public.users (nameuser, password) VALUES ('demo','demo');",
+     (err:Error) => {if (err){
+    console.log("- ☺ INSERT users Clfame error: " + err.message)}else{
+    console.log("+ ☺ INSERT users Clfame complite")
+    console.log("+ ☺"+ connection.query("SELECT * FROM public.users"));}});
 
+const store: { value: any[] } = {
+    value: []
+};
+
+const server = express();
+
+server.get('/store', (request: Request, response: Response) => {
+    response.json(JSON.stringify(store));
+    response.end();
+})
+
+server.get('/addToStore/:data', (request: Request, response: Response) => {
+    const params = request.params as { data: string };
+    store.value.push(params.data)
+    response.end();
+})
 
 server.get('/', (request: Request, response: Response) => {
     response.sendFile(path.resolve('./index.html'));
@@ -50,5 +80,5 @@ server.get('/v1/user/:id', (request: Request, response: Response) => {
 })
 
 server.listen(8080, () => {
-    console.log("Сервер поехал");
+    console.log("+ ☺ run server Clfame");
 });
