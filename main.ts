b6fd022295abcdcadbe3e7b044ef29c8939c84d8
zareyn <137310368+zareyn@users.noreply.github.com>
2025-01-27 20:47:44 +0300

    debug main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,43 @@
 import express, { Request, Response } from "express"
+import { connect } from "http2";
+//import { Pool } from "mysql2/typings/mysql/lib/Pool";
 import path from "path";
-import mysql from "mysql";
+//import mysql from "mysql";
+import pg, { Client } from 'pg';
+ 
+const connection = new pg.Client({
+  port: 5444,
+  user: 'test',
+  password: 'test',
+  host: 'localhost',
+  database: 'test',
+})
 
+//const connection = mysql.createConnection({
+//  port: 5444,
+//  host: "localhost",
+//  user: "test",
+//  database: "test",
+//  password: "test",
+//}); 
+
+  connection.connect(err =>{
+    if (err) {
+     return console.error("- ☺ Server Clfame error: " + err.message);
+    }
+    else{
+      console.log("+ ☺ Server Clfame connect");
+    }
+});
+  connection.query("SELECT * FROM public.users", 
+    (err:Error) => { if (err){
+     console.log("- ☺ file users Clfame error: " + err.message)}else
+    {console.log("+ ☺ file users Clfame connect")}});
+ connection.query("INSERT INTO public.users (nameuser, password) VALUES ('demo','demo');",
+     (err:Error) => {if (err){
+    console.log("- ☺ INSERT users Clfame error: " + err.message)}else{
+    console.log("+ ☺ INSERT users Clfame complite")
+    console.log("+ ☺"+ connection.query("SELECT * FROM public.users"));}});
 
 const store: { value: any[] } = {
     value: []
@@ -44,5 +80,5 @@ server.get('/v1/user/:id', (request: Request, response: Response) => {
 })
 
 server.listen(8080, () => {
-    console.log("Сервер поехал");
+    console.log("+ ☺ run server Clfame");
 });
